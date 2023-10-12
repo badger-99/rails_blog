865796a25b7d542fca2b33bb3e0709f9f4871562
Alfred M <alfred.mkg@proton.me>
2023-10-12 18:19:13 +0300

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ApplicationRecord
   has_many :likes
   has_many :comments
 
+  validates :name, presence: true
+  validates :posts_counter, comparison: { greater_than_or_equal_to: 0 }
+
   scope :recent_users, -> { order(created_at: :desc).limit(3) }
 end
