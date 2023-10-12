94cc8f58f628cb1e66e45043ebe0418c3ae87b93
Alfred M <alfred.mkg@proton.me>
2023-10-12 15:13:14 +0300

    Update 'recent_comments' method

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,11 @@ class Post < ApplicationRecord
   belongs_to :user, foreign_key: 'author_id'
   has_many :likes
   has_many :comments
-  before_create :increment_user_post_count
+  after_create :increment_user_post_count
 
-  scope :recent_comments, -> { includes(:comments).order(created_at: :desc).limit(5) }
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 
   private
 
