c7c7dbd6823d4f095f7bdd23dc554c6d1dca6b50
Alfred M <alfred.mkg@proton.me>
2023-10-18 18:20:59 +0300

    Update user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
-  def index; end
+  def index
+    @users = User.order(created_at: :asc)
+  end
 
   def show
-    @user = params[:id]
+    @user = User.find(params[:id])
+    @posts = Post.where(author_id: params[:id]).order(created_at: :desc)
   end
 end
