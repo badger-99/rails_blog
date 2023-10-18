285f9920f0d92d69e612357825a86594b6cd390c
Alfred M <alfred.mkg@proton.me>
2023-10-18 19:51:51 +0300

    Update post_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,14 @@
 class PostsController < ApplicationController
   def index
-    @user = params[:user_id]
+    @user = User.find(params[:user_id])
+    @users = User.select(:id, :name)
+    @posts = Post.where(author_id: params[:user_id]).order(created_at: :desc)
   end
 
   def show
-    @user = params[:user_id]
-    @post = params[:id]
+    @user = User.find(params[:user_id])
+    @users = User.select(:id, :name)
+    @post = Post.find(params[:post_id])
+    @comments = Comment.where(post_id: params[:post_id]).order(created_at: :asc)
   end
 end
