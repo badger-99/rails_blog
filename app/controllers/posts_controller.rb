b7410b49161e89861455f98f81ebeab29e3b0aa0
Alfred M <alfred.mkg@proton.me>
2023-10-24 00:02:59 +0300

    Solve n+1 problems

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,6 @@ class PostsController < ApplicationController
 
   def index
     @user = User.find(params[:user_id])
-    @users = User.select(:id, :name)
     @posts = Post.where(author_id: params[:user_id]).paginate(page: params[:page], per_page: 2).order(created_at: :desc)
   end
 
