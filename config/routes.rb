36ca03577794e606bcbf8bd9b05443904ac92d77
Alfred M <alfred.mkg@proton.me>
2023-10-27 13:46:53 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
+    get '/users/password' => 'devise/passwords#update'
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # resources :users
