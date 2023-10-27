ed9c251a81c7b1c6143ea16f250abb0cf73d2460
Alfred M <alfred.mkg@proton.me>
2023-10-27 09:54:09 +0300

    Add strong parameters for registration forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_current_user
+  before_action :configure_registration_parameters, if: :devise_controller?
 
   def set_current_user
     @current_user = configure_current_user
+  end
+
+  protected
+
+  def configure_registration_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password)}
   end
 
   private
