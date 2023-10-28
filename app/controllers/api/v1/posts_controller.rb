86222dd961d516471f788240447101c8b3dd35fc
Mahdi Noori <mahdinoori2389@gmail.com>
2023-10-28 22:30:06 +0430

    Create endpoint for user to comment for a post

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -12,6 +12,22 @@ class Api::V1::PostsController < JSONAPI::ResourceController
     render json: comments, status: :ok
   end
 
+  def add_comment
+    post = Post.find(params[:post_id]) # Find the post by post_id parameter
+    user = current_user # Retrieve the currently signed-in user
+
+    # Create a new comment with the user's ID
+    comment = Comment.new(comment_params)
+    comment.user_id = user.id
+    comment.post_id = post.id # Set the post_id to associate the comment with the post
+
+    if comment.save
+      render json: comment, status: :created
+    else
+      render json: comment.errors, status: :unprocessable_entity
+    end
+  end
+  
   private
 
   def set_json_api_content_type
