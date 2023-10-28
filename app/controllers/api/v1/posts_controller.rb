ef7b82cbba07c9d90718704b03fbb50cc119d459
Alfred M <alfred.mkg@proton.me>
2023-10-28 20:16:56 +0300

    Create endpoint to see posts by a user

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,2 +1,7 @@
 class Api::V1::PostsController < ApplicationController
+  def user
+    user = User.find(params[:user_id]) # Find the user by user_id parameter
+    posts = user.posts # Get all posts by the user
+    render json: posts, status: :ok
+  end
 end
